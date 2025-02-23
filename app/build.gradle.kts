ca86cb89e35a9852730f03094337e85bdf82c602
seoyoon <syoonlee513@gmail.com>
2025-02-23 13:59:49 +0900

    chore: compileSdk 35로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.teamwizdum.wizdum"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.teamwizdum.wizdum"
--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.teamwizdum.wizdum.feature"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
