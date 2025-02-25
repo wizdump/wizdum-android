c6555ad7254612c72fac3475ebea70f1eacde583
seoyoon <syoonlee513@gmail.com>
2025-02-25 14:25:06 +0900

    feat: 네트워크 모듈, TokenRepository 구성

---
+++ data/src/main/java/com/teamwizdum/wizdum/data/di/NetworkModule.kt
@@ -0,0 +1,78 @@
+package com.teamwizdum.wizdum.data.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONArray
+import org.json.JSONObject
+import retrofit2.Retrofit
+import timber.log.Timber
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val TAG = "Retrofit"
+    private const val BASE_URL = "http://211.188.49.238:8080/"
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(
+        json: Json,
+    ): HttpLoggingInterceptor = HttpLoggingInterceptor { message ->
+        when {
+            !message.isJsonObject() && !message.isJsonArray() ->
+                Timber.tag(TAG).d("CONNECTION INFO -> $message")
+
+            else -> kotlin.runCatching {
+                json.encodeToString(Json.parseToJsonElement(message))
+            }.onSuccess {
+                Timber.tag(TAG).d(it)
+            }.onFailure {
+                Timber.tag(TAG).d(message)
+            }
+        }
+    }.apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideJson(): Json {
+        return Json {
+            prettyPrint = true
+            coerceInputValues = true
+            ignoreUnknownKeys = true
+            encodeDefaults = true
+        }
+    }
+}
+
+fun String.isJsonObject(): Boolean = runCatching { JSONObject(this) }.isSuccess
+
+fun String.isJsonArray(): Boolean = runCatching { JSONArray(this) }.isSuccess
