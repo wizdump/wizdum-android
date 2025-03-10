0b61a2fc72bd914e2807a58763c4602b97c206d7
seoyoon <syoonlee513@gmail.com>
2025-03-10 20:31:34 +0900

    feat: 로그인 완료, 매칭 진행중 화면 추가

---
+++ feature/src/main/java/com/teamwizdum/wizdum/feature/login/LoginSuccessScreen.kt
@@ -0,0 +1,61 @@
+package com.teamwizdum.wizdum.feature.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamwizdum.wizdum.designsystem.theme.Green200
+import com.teamwizdum.wizdum.designsystem.theme.WizdumTheme
+import com.teamwizdum.wizdum.feature.R
+
+@Composable
+fun LoginSuccessScreen(name: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_checked),
+            contentDescription = "체크",
+            modifier = Modifier.size(32.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Green200)) {
+                    append(name)
+                }
+                append("님 반가워요!")
+            },
+            style = WizdumTheme.typography.h2
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "멘토님이 대화할 준비를 하고 있어요!", style = WizdumTheme.typography.body1)
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 800)
+@Composable
+fun LoginSuccessScreenPreview() {
+    WizdumTheme {
+        LoginSuccessScreen("유니")
+    }
+}
---
+++ feature/src/main/java/com/teamwizdum/wizdum/feature/onboarding/MatchingInProgressScreen.kt
@@ -0,0 +1,80 @@
+package com.teamwizdum.wizdum.feature.onboarding
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamwizdum.wizdum.designsystem.theme.Black200
+import com.teamwizdum.wizdum.designsystem.theme.Green200
+import com.teamwizdum.wizdum.designsystem.theme.WizdumTheme
+
+@Composable
+fun MatchingInProgressScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(76.dp)
+                    .background(color = Color.White, shape = CircleShape)
+                    .border(width = 3.dp, color = Black200, shape = CircleShape),
+            )
+            Box(
+                modifier = Modifier
+                    .padding(start = 57.dp)
+                    .width(27.dp)
+                    .height(8.dp)
+                    .background(color = Green200)
+            )
+        }
+
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "시간의 문이 열리는 중...", style = WizdumTheme.typography.body2)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Green200)) {
+                    append("운명적인 멘토")
+                }
+                append("와 연결 중이에요!")
+            },
+            style = WizdumTheme.typography.h3_semib
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 800)
+@Composable
+fun MatchingInProgressScreenPreview() {
+    WizdumTheme {
+        MatchingInProgressScreen()
+    }
+}
