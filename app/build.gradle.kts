b2a8d3b36686f06046e1519d63f2becdda5002fb
seoyoon <syoonlee513@gmail.com>
2025-03-27 15:41:36 +0900

    release: 1.0.01 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.teamwizdum.wizdum"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
